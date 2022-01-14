b07567e241b7b9f0532eeb59d23e3efc2f3e2cba
micah <micah@aatbio.com>
2022-01-14 22:49:24 +0000

    updated/buggy latest iteration

--- MolarityMolCalculator/MolarityMolCalculator.tsx
+++ MolarityMolCalculator/MolarityMolCalculator.tsx
@@ -1,74 +1,140 @@
-import React, { useState, KeyboardEvent, KeyboardEventHandler } from 'react';
+import React, { useState, KeyboardEvent, KeyboardEventHandler, useEffect } from 'react';
 import { RoundSigFig } from './Rounding';
 import './MolarityMolCalculator.css';
 
+type compoundsType = {
+    [key: string]: string | number | boolean;
+    name: string | "",
+    molecularFormula: string | "",
+    mw: number | "",
+    mass: number | "",
+    moles: number | "",
+    molarity: number | "",
+    cid: number | "",
+    valid: boolean | ""
+}
+
+const compounds: compoundsType = {
+    name: "",
+    molecularFormula: "",
+    mw: "",
+    mass: "",
+    moles: "",
+    molarity: "",
+    cid: "",
+    valid: ""
+}
+
 type MolarityMolDataType = {
     volume: number | "",
-    unitVolume: string
-    unitMass: string,
-    unitMolarity: string,
-    compounds: {
-        name: string | "",
-        mw: number | "",
-        mass: number | "",
-        moles: number | "",
-        molarity: number | ""
-    }
+    unitVolume: number,
+    unitMass: number,
+    unitMolarity: number,
+    compounds: Array<compoundsType>
 }
 
 const initial: MolarityMolDataType = {
     volume: 1,
-    unitVolume: "1",
-    unitMass: "1",
-    unitMolarity: "1",
-    compounds: {
-        name: "",
-        mw: "",
-        mass: "",
-        moles: "",
-        molarity: ""
-    }
+    unitVolume: 1,
+    unitMass: 1,
+    unitMolarity: 1,
+    compounds: []
 }
 
 const volDropMenu = [
-    { name: "L", value: "1", tag: "unitVolume" },
-    { name: "mL", value: "1000", tag: "unitVolume" },
-    { name: "µL", value: "1000000", tag: "unitVolume" }
+    { name: "L", value: 1, tag: "unitVolume" },
+    { name: "mL", value: .001, tag: "unitVolume" },
+    { name: "µL", value: .000001, tag: "unitVolume" }
 ]
 const massDropMenu = [
-    { name: "g", value: "1", tag: "unitMass" },
-    { name: "mg", value: "1000", tag: "unitMass" },
-    { name: "µg", value: "1000000", tag: "unitMass" }
+    { name: "g", value: 1, tag: "unitMass" },
+    { name: "mg", value: .001, tag: "unitMass" },
+    { name: "µg", value: .000001, tag: "unitMass" }
 ]
 
 const molarityDropMenu = [
-    { name: "M", value: "1", tag: "unitMolarity" },
-    { name: "mM", value: "1000", tag: "unitMolarity" },
-    { name: "µM", value: "1000000", tag: "unitMolarity" }
+    { name: "M", value: 1, tag: "unitMolarity" },
+    { name: "mM", value: .001, tag: "unitMolarity" },
+    { name: "µM", value: .000001, tag: "unitMolarity" }
 ]
 
 const MolarityMolCalculator = (props: any) => {
 
     const [data, _data] = useState<MolarityMolDataType>(initial);
 
+    useEffect(() => {
+        const w: Array<compoundsType> = [];
+        for (let i = 0; i < 3; i++) {
+            const c = { ...compounds }
+            w.push(c);
+        }
+        _data((data) => {
+            data.compounds = w;
+            return { ...data }
+        })
+    }, [])
+
+    useEffect(() => {
+        for (let i = 0, imax = data.compounds.length; i < imax; i++) {
+            let value: number | "";
+            if (data.compounds[i].mw === "" || data.compounds[i].mass === "") {
+                value = "";
+            }
+            else {
+                value = calculateMoles(data.compounds[i].mass, data.unitMass, data.compounds[i].mw);
+            }
+            _data((data) => {
+                data.compounds[i].moles = value;
+                return { ...data }
+            })
+        }
+    }, [data.unitMass, data.compounds])
+
+    useEffect(() => {
+        for (let i = 0, imax = data.compounds.length; i < imax; i++) {
+            let value: number | "";
+            if (data.volume === "" || data.compounds[i].moles === "") {
+                value = "";
+            }
+            else {
+                value = calculateMolarity(data.compounds[i].moles, data.unitVolume, data.volume, data.unitMolarity);
+            }
+            _data((data) => {
+                data.compounds[i].molarity = value;
+                return { ...data }
+            })
+        }
+    }, [data.compounds, data.volume, data.unitVolume, data.unitMolarity])
+
+    useEffect(() => {
+        const lastEntry = data.compounds[data.compounds.length - 1];
+        if (lastEntry.moles !== "" || lastEntry.molarity !== "") {
+            const w: Array<compoundsType> = [...data.compounds];
+            const newCompound = { ...compounds }
+            w.push(newCompound)
+            _data((data) => {
+                data.compounds = w;
+                return { ...data }
+            })
+        }
+    }, [data.compounds])
+
     return (
         <>
             <RenderPageContent
                 handleInput={handleInput}
+                handleTableInput={handleTableInput}
                 preventMinus={preventMinus}
                 _data={_data}
                 data={data}
             />
-            {console.log("The volume is: " + data.volume)}
-            {console.log("The unitVolume is: " + data.unitVolume)}
-            {console.log("The unitMass is: " + data.unitMass)}
-            {console.log("The unitMolarity is: " + data.unitMolarity)}
         </>
     )
 }
+
 /**
  * Prevents input from being negative by stopping both keyboard inputs for negative.
- * @param ev 
+ * @param ev
  */
 const preventMinus = (ev: KeyboardEvent) => {
     if (ev.code === 'Minus' || ev.code === 'NumpadSubtract') {
@@ -76,8 +142,7 @@ const preventMinus = (ev: KeyboardEvent) => {
     }
 }
 
-const handleInput = (ev: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>
-    , data: MolarityMolDataType) => {
+const handleInput = (ev: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>) => {
     const tag = ev.target.getAttribute('data-tag');
     const value = ev.target.value;
     _data((data) => {
@@ -85,20 +150,92 @@ const handleInput = (ev: any, _data: React.Dispatch<React.SetStateAction<Molarit
     })
 }
 
-const handleDropDownMenu = (event: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>) => {
-    const value = event.target.value;
-    const tag = event.target[event.target.selectedIndex].getAttribute('data-tag');
+/**
+ * Notes: should be a change effect. input debouncing 250ms timeout.
+ */
+const handleName = (ev: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>, data: MolarityMolDataType) => {
+    const index = ev.target.getAttribute('data-attribute');
+    fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${data.compounds[index].name}/property/MolecularWeight,MolecularFormula,IUPACName/json`)
+        .then(response => {
+            if (!response.ok) {
+                _data((data) => {
+                    data.compounds[index].valid = false;
+                    data.compounds[index].mw = "";
+                    data.compounds[index].mf = "";
+                    return { ...data, compounds: [...data.compounds] }
+                })
+                throw response;
+            }
+            return response.json();
+        })
+        .then(users => {
+            const cid = users['PropertyTable']['Properties'][0]['CID'];
+            const mw = users['PropertyTable']['Properties'][0]['MolecularWeight'];
+            const mf = users['PropertyTable']['Properties'][0]['MolecularFormula'];
+
+            _data((data) => {
+                data.compounds[index].valid = true;
+                data.compounds[index].mw = mw;
+                data.compounds[index].molecularFormula = mf;
+                data.compounds[index].cid = cid;
+                return { ...data, compounds: [...data.compounds] }
+            })
+        })
+}
+
+/*
+const debounce = (func: Function, wait: number) => {
+    let timeout: any;
+
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
+*/
+const handleTableInput = (ev: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>) => {
+    const tag = ev.target.getAttribute('data-tag');
+    const value = ev.target.value;
+    const index = ev.target.getAttribute('data-attribute')
+
+    _data((data) => {
+        data.compounds[index][tag] = value;
+        return { ...data, compounds: [...data.compounds] }
+    })
+}
+
+const handleDropDownMenu = (ev: any, _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>) => {
+    const value = ev.target.value;
+    const tag = ev.target[ev.target.selectedIndex].getAttribute('data-tag');
     _data((data) => {
         return { ...data, [tag]: value }
     })
 }
 
-const recalculateAll = (event: any) => {
-    //code for calculating new molarity based on new input of volume solution
+const calculateMoles = (mass: number | "", unitMass: number, mw: number | "") => {
+    if (mass !== "" && mw !== "") {
+        const moles = RoundSigFig(((mass * unitMass) / mw), 3);
+        return moles;
+    }
+    else { return 0; }
+}
+
+const calculateMolarity = (moles: number | "", volume: number | "", unitVolume: number, unitMolarity: number) => {
+    if (moles !== "" && volume !== "") {
+        const molarity = RoundSigFig((moles / (volume * unitVolume * unitMolarity)), 3);
+        return molarity;
+    }
+    else { return 0; }
 }
 
 const RenderPageContent = (props: {
     handleInput: Function,
+    handleTableInput: Function,
     preventMinus: KeyboardEventHandler,
     _data: React.Dispatch<React.SetStateAction<MolarityMolDataType>>,
     data: MolarityMolDataType
@@ -171,7 +308,54 @@ const RenderPageContent = (props: {
                         </thead>
                         <tbody>
                             {
-
+                                data.compounds.map((props, index) => {
+                                    return (
+                                        <tr>
+                                            <td><input
+                                                type="text"
+                                                value={props.name}
+                                                data-tag="name"
+                                                data-attribute={index}
+                                                onChange={(event: any) => {
+                                                    handleName(event, _data, data);
+                                                    handleTableInput(event, _data)
+                                                }} /></td >
+                                            <td>
+                                                {(data.compounds[index].name === "") ? "-" :
+                                                    (data.compounds[index].valid === false) ? "No matching compounds" :
+                                                        <a style={{ textDecoration: "none" }} href={"https://pubchem.ncbi.nlm.nih.gov/compound/" + data.compounds[index].cid}>{data.compounds[index].molecularFormula}</a>}
+                                            </td>
+                                            <td><input
+                                                type="number"
+                                                value={props.mw}
+                                                data-tag="mw"
+                                                data-attribute={index}
+                                                onChange={(event: any) => { handleTableInput(event, _data) }}
+                                            /></td>
+                                            <td><input
+                                                type="number"
+                                                value={props.mass}
+                                                data-tag="mass"
+                                                data-attribute={index}
+                                                onChange={(event: any) => { handleTableInput(event, _data) }}
+                                            /></td>
+                                            <td><input
+                                                type="number"
+                                                value={props.moles}
+                                                data-tag="moles"
+                                                data-attribute={index}
+                                                disabled
+                                            /></td>
+                                            <td><input
+                                                type="number"
+                                                value={props.molarity}
+                                                data-tag="molarity"
+                                                data-attribute={index}
+                                                disabled
+                                            /></td>
+                                        </tr>
+                                    )
+                                })
                             }
                         </tbody>
                     </table>
